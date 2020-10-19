e0ac87bb0607c2aecb708a9916f9202db50c296b
VictorineR <victorine.richard@efrei.net>
2020-10-19 12:40:17 +0200

    Data

---
+++ efrei-3a-web-Le-Chevalet-master/server/data/abtrait.js
@@ -0,0 +1,84 @@
+const abtrait = [
+    {
+      id: 1,
+      name: 'Abstract Painting',
+      painter: 'Gerhard Richter',
+      date: '2016',
+      image: 'https://blog.artsper.com/wp-content/uploads/2018/12/Gerhard-Richter-Abstraktes-Bild-946-3.jpg',
+      price: 100000
+    },
+    {  
+        id: 2,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    },
+    {  
+        id: 3,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    },
+    {
+        id: 4,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    },
+    {
+        id: 5,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    },
+    {
+        id: 6,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    },
+    {
+        id: 7,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    },
+    {
+        id: 8,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    },
+    {
+        id: 9,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    },
+    {
+        id: 10,
+        name: 'Sans Titre',
+        painter: 'Zao Wou-Ki',
+        date: '1969',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        price: 200000
+    }
+  ]
+  
+  module.exports = abtrait
