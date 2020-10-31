b021bcd3fa2d3c834729c20e04b89b4927bd389a
VictorineR <victorine.richard@efrei.net>
2020-10-31 18:02:17 +0100

    home début

--- efrei-3a-web-Le-Chevalet-master/server/data/abtrait.js
+++ efrei-3a-web-Le-Chevalet-master/server/data/abtrait.js
@@ -9,7 +9,7 @@ const abtrait = [
     },
     {  
         id: 2,
-        name: 'Sans Titre',
+        name: 'Untitled',
         painter: 'Zao Wou-Ki',
         date: '1969',
         image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
@@ -17,66 +17,66 @@ const abtrait = [
     },
     {  
         id: 3,
-        name: 'Sans Titre',
-        painter: 'Zao Wou-Ki',
-        date: '1969',
-        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        name: 'Untitled',
+        painter: 'Cy Twombly',
+        date: '2008',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/T14081_10.jpg',
         price: 200000
     },
-    {
+    {  
         id: 4,
-        name: 'Sans Titre',
-        painter: 'Zao Wou-Ki',
-        date: '1969',
-        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        name: 'Congress',
+        painter: 'Julie Mehretu',
+        date: '2003',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/mehretu_congress.jpg',
         price: 200000
     },
-    {
+    { 
         id: 5,
-        name: 'Sans Titre',
-        painter: 'Zao Wou-Ki',
-        date: '1969',
-        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        name: 'Darlinghurst Cats',
+        painter: 'John Olsen',
+        date: '2018',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/22600.jpg',
         price: 200000
     },
-    {
+    { 
         id: 6,
-        name: 'Sans Titre',
-        painter: 'Zao Wou-Ki',
-        date: '1969',
-        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        name: 'Cardinal',
+        painter: 'Jean-Marc Bustamante',
+        date: '2010',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/JBU_1022_original-1_large.jpg',
         price: 200000
     },
     {
         id: 7,
-        name: 'Sans Titre',
-        painter: 'Zao Wou-Ki',
-        date: '1969',
-        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        name: 'Challenger',
+        painter: 'John M Armleder',
+        date: '2016',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/almine-rech-gallery-challenger-ja003316167jpg.jpg',
         price: 200000
     },
     {
         id: 8,
-        name: 'Sans Titre',
-        painter: 'Zao Wou-Ki',
+        name: 'Harran II',
+        painter: ' Frank Stella',
         date: '1969',
-        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/frank_stella_de_young_museum_5_ter.jpg',
         price: 200000
     },
     {
         id: 9,
-        name: 'Sans Titre',
-        painter: 'Zao Wou-Ki',
-        date: '1969',
-        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        name: ' Bracket',
+        painter: 'Joan Mitchell',
+        date: '1989',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/Joan-MItchell-BRACKET-002.jpg',
         price: 200000
-    },
-    {
+    }, 
+    {  
         id: 10,
-        name: 'Sans Titre',
-        painter: 'Zao Wou-Ki',
-        date: '1969',
-        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/1.jpg',
+        name: 'Composition Zulma',
+        painter: 'Christian Bonnefoi',
+        date: '2017',
+        image: 'https://blog.artsper.com/wp-content/uploads/2018/12/Composition-Zulma-900x280-2017-1-644x216.jpg',
         price: 200000
     }
   ]
