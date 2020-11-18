3cc1ac7981a1c297296faa972c82021d0421215a
Victor Garnier <victor.garnier@efrei.net>
2020-11-18 20:43:59 +0100

    Option boutique en local tout marche

--- efrei-3a-web-Le-Chevalet-master/client/components/Collection.vue
+++ efrei-3a-web-Le-Chevalet-master/client/components/Collection.vue
@@ -68,7 +68,7 @@
 
     <div class="content-collection" id="content-collection">
       <article v-for="article in articles" :key="article.id">
-        <div class="article-content">
+        <div class="article-content" v-if="editingArticle.id !== article.id">
           <div class="article-title">
             <div class="article-img">
               <div
@@ -101,10 +101,61 @@
               <button class="button" @click="deleteArticle(article.id)">
                 Supprimer
               </button>
+              <button class="button" @click="editArticle(article)">
+                Modifier
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="article-content" v-else>
+          <div class="article-title">
+            <input
+            type="text"
+            v-model="editingArticle.image"
+            placeholder="Lien vers l'image"
+          />
+            <h2>
+              <input type="text" v-model="editingArticle.name" /> -
+              <input type="number" v-model="editingArticle.price" />
+            </h2>
+            <p><textarea v-model="editingArticle.description"></textarea></p>
+
+            <div>
+              <button @click="sendEditArticle()">Valider</button>
+              <button @click="abortEditArticle()">Annuler</button>
             </div>
           </div>
         </div>
       </article>
+    </div>
+    <div class="formulaire">
+      <form @submit.prevent="addArticle">
+        <h2>Nouveau produit à ajouter</h2>
+        <input
+          type="text"
+          v-model="newArticle.name"
+          placeholder="Nom du produit"
+          required
+        />
+        <input
+          type="number"
+          v-model="newArticle.price"
+          placeholder="Prix"
+          required
+        />
+        <textarea
+          type="text"
+          v-model="newArticle.description"
+          placeholder="Description du produit"
+          required
+        ></textarea>
+        <input
+          type="text"
+          v-model="newArticle.image"
+          placeholder="Lien vers l'image"
+        />
+        <button type="submit">Ajouter</button>
+      </form>
     </div>
   </div>
 </template>
@@ -115,7 +166,29 @@ module.exports = {
     articles: { type: Array, default: [] },
     panier: { type: Object },
   },
-  methods: {
+  data() {
+    return {
+      newArticle: {
+        name: "",
+        description: "",
+        image: "",
+        price: 0,
+      },
+      editingArticle: {
+        id: -1,
+        name: "",
+        description: "",
+        image: "",
+        price: 0,
+      },
+      isActivated: false,
+    };
+  },
+
+    methods: {
+    addArticle() {
+      this.$emit("add-article", this.newArticle);
+    },
     addToPanier(articleId) {
       this.$emit("add-to-panier", articleId, this.article);
     },
@@ -124,6 +197,26 @@ module.exports = {
     },
     deleteArticle(articleId) {
       this.$emit("delete-article", articleId);
+    },
+    editArticle(article) {
+      this.editingArticle.id = article.id;
+      this.editingArticle.name = article.name;
+      this.editingArticle.description = article.description;
+      this.editingArticle.image = article.image;
+      this.editingArticle.price = article.price;
+    },
+    sendEditArticle() {
+      this.$emit("update-article", this.editingArticle);
+      this.abortEditArticle();
+    },
+    abortEditArticle() {
+      this.editingArticle = {
+        id: -1,
+        name: "",
+        description: "",
+        image: "",
+        price: 0,
+      };
     },
   },
 };
