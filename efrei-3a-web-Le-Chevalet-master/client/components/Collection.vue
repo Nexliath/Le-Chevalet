689318a6d955717b8812a3a2ab70875a3b76a29d
Victor Garnier <victor.garnier@efrei.net>
2020-11-20 01:49:58 +0100

    css boutique pratiquement fini

--- efrei-3a-web-Le-Chevalet-master/client/components/Collection.vue
+++ efrei-3a-web-Le-Chevalet-master/client/components/Collection.vue
@@ -53,68 +53,39 @@
     </section>
     <!-- collection-cover Section  -->
 
-    <!-- description of the collection  -->
+    
 
-    <!-- End description of the collection  -->
+    
 
     <div class="boutique">
       <div class="intro">
         <h1>Pa<span>i</span>nting l<span>i</span>st</h1>
+        <!-- description of the collection  -->
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
           deleniti maiores pariatur assumenda quas magni et, doloribus quod
           voluptate quasi molestiae magnam officiis dolorum, dolor provident
           atque molestias voluptatum explicabo!
         </p>
+        <!-- End description of the collection  -->
       </div>
-
-      <div v-for="tableau in tableaux" :key="tableau.id" class="container-painting">
+      <!-- Beginning of the shop layout  -->
+      <div
+        v-for="tableau in tableaux"
+        :key="tableau.id"
+        class="container-painting"
+      >
         <div class="content">
-          <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
-            <div class="content-overlay"></div>
-            <img class="content-image" :src="tableau.image" />
-            <div class="content-details fadeIn-top">
-              <h3>{{ tableau.name }} | {{ tableau.date }}</h3>
-              <p>By {{ tableau.painter }}, movement: {{ tableau.movement }}</p>
-            </div>
-          </a>
-        </div>
-        <div class="content-button">
-          <button class="delete" @click="deleteTableau(tableau.id)">
-            Delete
-          </button>
-          <button class="modify" @click="editTableau(tableau)">Modify</button>
-          <button
-            class="remove-basket"
-            v-if="isInPanier(tableau.id)"
-            @click="removeFromPanier(tableau.id)"
-          >
-            Delete from basket
-          </button>
-          <button
-            class="add-basket"
-            v-else="isInPanier(tableau.id)"
-            @click="addToPanier(tableau.id)"
-          >
-            Add to basket
-          </button>
-        </div>
-      </div>
-
-      <article v-for="tableau in tableaux" :key="tableau.id">
-        <div class="article-img">
+          <div class="content-overlay"></div>
+          <img class="content-image" :src="tableau.image" />
           <div
-            class="article"
-            :style="{ backgroundImage: 'url(' + tableau.image + ')' }"
-          ></div>
-        </div>
-        <div class="article-content" v-if="editingTableau.id !== tableau.id">
-          <div class="article-title">
-            <h2>
-              {{ tableau.name }}, {{ tableau.movement }} - {{ tableau.price }}€
-            </h2>
-            <p>{{ tableau.painter }}, {{ tableau.date }}</p>
-            <div>
+            class="content-details fadeIn-top"
+            v-if="editingTableau.id !== tableau.id"
+          >
+            <h3>{{ tableau.name }} | {{ tableau.date }}</h3>
+            <p>By {{ tableau.painter }}, movement: {{ tableau.movement }}</p>
+            <p>Price: {{ tableau.price }} €</p>
+            <div class="button-action">
               <button class="delete" @click="deleteTableau(tableau.id)">
                 Delete
               </button>
@@ -137,107 +108,97 @@
               </button>
             </div>
           </div>
-        </div>
-        <div class="article-content" v-else>
-          <div class="article-title">
-            <h2>
-              <input type="text" v-model="editingTableau.name" />,
-              <input type="text" v-model="editingTableau.movement" /> -
-              <input type="number" v-model="editingTableau.price" />
-            </h2>
-            <div>
-              <button class="add-basket" @click="sendEditTableau()">
-                Confirm
-              </button>
+          <!-- End of the shop layout  -->
+          <!-- Beginning of the shop modify layout  -->
+          <div class="content-details fadeIn-top" v-else>
+            <p>
+              <input type="text" v-model="editingTableau.name" /> |
+              <input type="number" v-model="editingTableau.date" />
+            </p>
+            <p>
+              <input type="text" v-model="editingTableau.painter" />,
+              <input type="text " v-model="editingTableau.movement" />
+            </p>
+            <p><input type="number" v-model="editingTableau.price" /> €</p>
+            <div class="button-action">
               <button class="delete" @click="abortEditTableau()">
                 Discard
               </button>
+              <button class="add-basket" @click="sendEditTableau()">
+                Confirm
+              </button>
             </div>
           </div>
-          <p>
-            <input
-              type="text"
-              v-model="editingTableau.painter"
-              placeholder="Painter name"
-            />,
-            <input
-              type="number"
-              v-model="editingTableau.date"
-              placeholder="Date of the tableau"
-            />
-          </p>
-
-          <input
-            type="text"
-            v-model="editingTableau.image"
-            placeholder="Lien vers l'image"
-          />
+          <!-- End of the shop modify layout  -->
         </div>
-      </article>
-      <div class="container-form">
-        <form id="newPainting" @submit.prevent="addTableau">
-          <h3>Add a new paint<span>i</span>ng</h3>
-          <h4>Please fill this form</h4>
-          <fieldset>
-            <input
-              placeholder="Painting's name"
-              v-model="newTableau.name"
-              type="text"
-              tabindex="1"
-              required
-              autofocus
-            />
-          </fieldset>
-          <fieldset>
-            <input
-              placeholder="Painter's name"
-              v-model="newTableau.painter"
-              type="text"
-              tabindex="2"
-              required
-            />
-          </fieldset>
-          <fieldset>
-            <input
-              placeholder="Movement type"
-              v-model="newTableau.movement"
-              type="text"
-              tabindex="3"
-              required
-            />
-          </fieldset>
-          <fieldset>
-            <input
-              placeholder="Painting's date"
-              v-model="newTableau.date"
-              type="number"
-              tabindex="4"
-              required
-            />
-          </fieldset>
-          <fieldset>
-            <input
-              placeholder="Painting's link picture"
-              v-model="newTableau.image"
-              type="text"
-              tabindex="5"
-              required
-            />
-          </fieldset>
-          <fieldset>
-            <input
-              placeholder="Price"
-              v-model="newTableau.price"
-              type="number"
-              tabindex="6"
-              required
-            />
-          </fieldset>
-          <fieldset>
-            <button type="submit">Add the painting</button>
-          </fieldset>
-        </form>
       </div>
+    </div>
+
+    <!-- Beginning of the add painting form  -->
+    <div class="container-form">
+      <form id="newPainting" @submit.prevent="addTableau">
+        <h3>Add a new paint<span>i</span>ng</h3>
+        <h4>Please fill this form</h4>
+        <fieldset>
+          <input
+            placeholder="Painting's name"
+            v-model="newTableau.name"
+            type="text"
+            tabindex="1"
+            required
+            autofocus
+          />
+        </fieldset>
+        <fieldset>
+          <input
+            placeholder="Painter's name"
+            v-model="newTableau.painter"
+            type="text"
+            tabindex="2"
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <input
+            placeholder="Movement type"
+            v-model="newTableau.movement"
+            type="text"
+            tabindex="3"
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <input
+            placeholder="Painting's date"
+            v-model="newTableau.date"
+            type="number"
+            tabindex="4"
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <input
+            placeholder="Painting's link picture"
+            v-model="newTableau.image"
+            type="text"
+            tabindex="5"
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <input
+            placeholder="Price"
+            v-model="newTableau.price"
+            type="number"
+            tabindex="6"
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <button type="submit">Add the painting</button>
+        </fieldset>
+      </form>
+      <!-- End of the add painting form  -->
     </div>
   </div>
 </template>
