166b3c9cad7aeaead9f32cd8510cf2a93ce0e0d3
Victor Garnier <victor.garnier@efrei.net>
2020-11-21 01:00:23 +0100

    Panier css pratiquement fini

--- efrei-3a-web-Le-Chevalet-master/client/components/Panier.vue
+++ efrei-3a-web-Le-Chevalet-master/client/components/Panier.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="all">
-    
     <section id="header">
       <div class="header container">
         <div class="nav-bar">
@@ -14,53 +13,107 @@
               <div class="bar"></div>
             </div>
             <ul>
-              <li><a href="shop.html#/collection" data-after="Service"><i class="fas fa-palette"></i> Collection</a>
-              <li><a href="shop.html#/panier" data-after="paintings"><i class="fas fa-shopping-cart" id="here"></i> Panier</a></li>
-              <li><a href="shop.html#/login" data-after="paintings"><i class="fas fa-user-alt"></i> Connexion</a></li>
-              <li><a href="aboutus.html" data-after="About"><i class="fas fa-gavel" ></i> About</a></li>
+              <li>
+                <a href="shop.html#/collection" data-after="Service"
+                  ><i class="fas fa-palette"></i> Collection</a
+                >
+              </li>
+              <li>
+                <a href="shop.html#/panier" data-after="paintings"
+                  ><i class="fas fa-shopping-cart" id="here"></i> Panier</a
+                >
+              </li>
+              <li>
+                <a href="shop.html#/login" data-after="paintings"
+                  ><i class="fas fa-user-alt"></i> Connexion</a
+                >
+              </li>
+              <li>
+                <a href="aboutus.html" data-after="About"
+                  ><i class="fas fa-gavel"></i> About</a
+                >
+              </li>
             </ul>
           </div>
         </div>
       </div>
     </section>
 
-  <!-- about-us-cover Section  -->
+    <!-- about-us-cover Section  -->
     <section id="panier-cover">
       <div class="home-cover container">
         <div>
-          <h1>Content panier <span></span></h1> <br>
+          <h1>content panier <span></span></h1>
+          <br />
           <a href="#article-was" type="button" class="cta">Access to it</a>
         </div>
       </div>
     </section>
-  <!-- End about-us-cover Section  -->
+    <!-- End about-us-cover Section  -->
 
-    <article v-for="tableau in tableaux_panier" :key="tableau.id">
-      <div class="article-img">
-        <div class="article" :style="{ backgroundImage: 'url(' + tableau.image + ')' }">
-        </div>
+    <div class="boutique">
+      <div class="intro">
+        <h1>Pa<span>i</span>nting l<span>i</span>st</h1>
+        <!-- description of the collection  -->
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
+          deleniti maiores pariatur assumenda quas magni et, doloribus quod
+          voluptate quasi molestiae magnam officiis dolorum, dolor provident
+          atque molestias voluptatum explicabo!
+        </p>
+        <p>
+          Total Price: 156181 € <br><button class="reservation">Reserver</button>
+        </p>
+        
+        <!-- End description of the collection  -->
       </div>
-      <div class="article-content" v-if="editingTableau.id !== tableau.id">
-        <div class="article-title">
-          <h2>{{ tableau.name }}, {{ tableau.date }} - {{ tableau.price }}€ x {{ tableau.quantity }}</h2>
-        </div>
-        <p class="description">{{ tableau.painter }}, {{ tableau.movement }}</p>
-        <div>
-          <button class="delete" @click="removeFromPanier(tableau.id)">Remove from basket</button>
-          <button class="modify" @click="editTableau(tableau)">Modify</button>
-        </div>
-      </div>
-      <div class="article-content" v-else>
-        <div class="article-title">
-          <h2>{{ tableau.name }} - {{ tableau.price }}€ x <input type="number" v-model="editingTableau.quantity"></h2>
-          <div>
-            <button class="validate" @click="sendEditQuantity()">Valider</button>
-            <button class="delete" @click="abortEditQuantity()">Annuler</button>
+      <!-- Beginning of the shop layout  -->
+      <div
+        v-for="tableau in tableaux_panier"
+        :key="tableau.id"
+        class="container-panier"
+      >
+        <div class="content-panier">
+          <div class="content-overlay-panier"></div>
+          <img class="content-image" :src="tableau.image" />
+          <div
+            class="content-details fadeIn-top"
+            v-if="editingTableau.id !== tableau.id"
+          >
+            <h3>{{ tableau.name }} | {{ tableau.painter }}</h3>
+            <p>Price: {{ tableau.price * tableau.quantity}} €</p>
+            <p>Quantity : {{tableau.quantity}}</p>
+            <div class="button-action">
+              <button class="modify" @click="editTableau(tableau)">
+                Modify Quantity
+              </button>
+              <button
+                class="remove-basket"
+                @click="removeFromPanier(tableau.id)"
+              >
+                Delete from basket
+              </button>
+            </div>
           </div>
+          <!-- End of the shop layout  -->
+          <!-- Beginning of the shop modify layout  -->
+          <div class="content-details fadeIn-top" v-else>
+            <h3>{{ tableau.name }} | {{ tableau.painter }}</h3>
+            <p>Price: {{ tableau.price * tableau.quantity}} €</p>
+            <p>Quantity : <input type="number" v-model="editingTableau.quantity"></p>
+            <div class="button-action">
+              <button class="validate" @click="abortEditQuantity()">
+                Discard
+              </button>
+              <button class="add-basket" @click="sendEditQuantity()">
+                Confirm
+              </button>
+            </div>
+          </div>
+          <!-- End of the shop modify layout  -->
         </div>
-        <p>{{ tableau.painter }}</p>
       </div>
-    </article>
+    </div>
   </div>
 </template>
 
@@ -74,39 +127,39 @@ module.exports = {
     return {
       editingTableau: {
         id: -1,
-        quantity: 0
-      }
-    }
+        quantity: 0,
+      },
+    };
   },
   computed: {
     tableaux_panier() {
-      return this.panier.tableaux.map(tableau => ({
+      return this.panier.tableaux.map((tableau) => ({
         ...tableau,
-        ...this.tableaux.find(a => a.id === tableau.id)
-      }))
-    }
+        ...this.tableaux.find((a) => a.id === tableau.id),
+      }));
+    },
   },
   methods: {
     pay() {
-      this.$emit('pay')
+      this.$emit("pay");
     },
     removeFromPanier(tableauId) {
-      this.$emit('remove-from-panier', tableauId)
+      this.$emit("remove-from-panier", tableauId);
     },
     editTableau(tableau) {
-      this.editingTableau.id = tableau.id
-      this.editingTableau.quantity = tableau.quantity
+      this.editingTableau.id = tableau.id;
+      this.editingTableau.quantity = tableau.quantity;
     },
     sendEditQuantity() {
-      this.$emit('update-panier', this.editingTableau)
-      this.abortEditQuantity()
+      this.$emit("update-panier", this.editingTableau);
+      this.abortEditQuantity();
     },
     abortEditQuantity() {
       this.editingTableau = {
         id: -1,
-        quantity: 0
-      }
-    }
-  }
-}
+        quantity: 0,
+      };
+    },
+  },
+};
 </script>
