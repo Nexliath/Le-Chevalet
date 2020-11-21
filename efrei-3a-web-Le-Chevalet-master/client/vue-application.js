21ff7382cabe0990f096a81d8580782d225cfc81
Victor Garnier <victor.garnier@efrei.net>
2020-11-21 22:47:09 +0100

    Vue-application.js comments

--- efrei-3a-web-Le-Chevalet-master/client/vue-application.js
+++ efrei-3a-web-Le-Chevalet-master/client/vue-application.js
@@ -3,7 +3,8 @@ const Panier = window.httpVueLoader('./components/Panier.vue')
 const Register = window.httpVueLoader('./components/Register.vue')
 const Login = window.httpVueLoader('./components/Login.vue')
 const About = window.httpVueLoader('./components/About.vue')
-const routes = [
+
+const routes = [ // those are all the routes we use for all website. Except the home page (index.html), all our pages are done with Vue.js 
   { path: '/collection', component: Collection },
   { path: '/panier', component: Panier },
   { path: '/register', component: Register},
@@ -32,7 +33,7 @@ var app = new Vue({
     this.loadUser()
   },
   methods: {
-    async loadTableaux() {
+    async loadTableaux() { // used to reload all paintings of the collection page
       try {
         const res = await axios.get('/api/tableaux')
         this.tableaux = res.data
@@ -40,7 +41,7 @@ var app = new Vue({
         alert("Error to load the paintings tableaux")
       }
     },
-    async loadPanier() {
+    async loadPanier() { // used to reload the basket (panier) of all paintings
       try {
         const res = await axios.get('/api/panier')
         this.panier = res.data
@@ -48,7 +49,7 @@ var app = new Vue({
         alert("Error to load the booking basket.")
       }
     },
-    async addTableau(tableau) {
+    async addTableau(tableau) { // used to add the wanted paintings to the basket
       try {
         const res = await axios.post('/api/tableau', tableau)
         this.tableaux.push(res.data)
@@ -56,7 +57,7 @@ var app = new Vue({
         alert("Error to create the painting.")
       }
     },
-    async updateTableau(newTableau) {
+    async updateTableau(newTableau) { // used to update the informations of wanted painting
       try {
         await axios.put('/api/tableau/' + newTableau.id, newTableau)
         const tableau = this.tableaux.find(a => a.id === newTableau.id)
@@ -71,7 +72,7 @@ var app = new Vue({
         alert("Error to modify the painting.")
       }
     },
-    async deleteTableau(tableauId) {
+    async deleteTableau(tableauId) { // used to update the informations of wanted painting
       try {
         await axios.delete('/api/tableau/' + tableauId)
         const index = this.tableaux.findIndex(a => a.id === tableauId)
@@ -80,7 +81,7 @@ var app = new Vue({
         alert("Error to delete the painting.")
       }
     },
-    async addToPanier(tableauId) {
+    async addToPanier(tableauId) { // used to add the wanted painting to the basket
       const tableau = {
         id: tableauId,
         quantity: 1
@@ -94,7 +95,7 @@ var app = new Vue({
         alert("Error to add the painting to bookings.")
       }
     },
-    async removeFromPanier(tableauId) {
+    async removeFromPanier(tableauId) { // used to remove the wanted painting from the basket
       try {
         await axios.delete('/api/panier/' + tableauId)
         const index = this.panier.tableaux.findIndex(a => a.id === tableauId)
@@ -104,7 +105,7 @@ var app = new Vue({
         alert("Error to remove the painting from bookings.")
       }
     },
-    async updatePanier(newTableau) {
+    async updatePanier(newTableau) { // used to add the wanted painting
       try {
         await axios.put('/api/panier/' + newTableau.id, newTableau)
         const tableau = this.panier.tableaux.find(a => a.id === newTableau.id)
@@ -120,21 +121,21 @@ var app = new Vue({
     async pay() {
       try {
         const res = await axios.post('/api/panier/pay')
-        this.panier.tableaux.splice(0, this.panier.tableaux.length)
+        this.panier.tableaux.splice(0, this.panier.tableaux.length) // we supress all the paintings of the baskets
         this.panier.updatedAt = new Date()
         alert("Thank you for booking")
       } catch (e) {
-        router.push('/login')
+        router.push('/login') // redirect to the login page if not connected
         alert("You need to be connected to book")
       }
     },
 
     async register(credentials) {
       try {
-        await axios.post('/api/register', credentials)
-        await axios.post('/api/login', credentials)
+        await axios.post('/api/register', credentials) // we call the register api
+        await axios.post('/api/login', credentials) // we call the login api
         await this.loadUser()
-        this.$forceUpdate()
+        this.$forceUpdate() // we reload the vue component
       } catch (e) {
         alert("Email already taken.")
       }
@@ -143,16 +144,16 @@ var app = new Vue({
       try {
         await axios.post('/api/login', credentials)
         await this.loadUser()
-        this.$forceUpdate()
+        this.$forceUpdate() // we reload the vue component
       } catch (e) {
         alert("E-mail or password incorrect.")
       }
     },
     async logout() {
       try {
-        await axios.post('/api/logout')
+        await axios.post('/api/logout') // we call the logout api
         this.user = null
-        this.$forceUpdate()
+        this.$forceUpdate() // we reload the vue component
       } catch (e) {
         alert("Not connected.")
       }
